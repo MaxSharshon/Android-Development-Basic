72279653bf3bd2849d6e53fc88f3123da88c215b
MaxSharshon <sharshonmax@gmail.com>
2024-11-18 20:20:57 +0200

    Add sorting algorithm

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -6,16 +6,19 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private var counter: Int = 0
     private lateinit var counterText: TextView
-
     private val receiver = CounterReceiver()
+    private val numbers = intArrayOf(5, 3, 8, 4, 2, 7, 1, 10, 6, 9)
 
     @SuppressLint("SetTextI18n")
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -38,6 +41,21 @@ class MainActivity : AppCompatActivity() {
             registerReceiver(receiver, filter, Context.RECEIVER_NOT_EXPORTED)
         }
 
+        findViewById<Button>(R.id.sortButton).setOnClickListener {
+            val startTime = System.currentTimeMillis()
+
+            // Start sorting in a coroutine (background thread)
+            Thread {
+                bubbleSort(numbers)
+                val endTime = System.currentTimeMillis()
+                val duration = endTime - startTime
+
+                // Show sorting duration in a Toast on the main thread
+                Handler(Looper.getMainLooper()).post {
+                    Toast.makeText(this@MainActivity, "Sort duration: $duration ms", Toast.LENGTH_SHORT).show() }
+            }.start()
+        }
+
         findViewById<Button>(R.id.goToSecondActivityButton).setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             intent.putExtra("counter", counter)
@@ -53,5 +71,18 @@ class MainActivity : AppCompatActivity() {
     fun updateCounter(newCounter: Int) {
         counter = newCounter
         counterText.text = counter.toString()
+    }
+
+    private fun bubbleSort(array: IntArray) {
+        val n = array.size
+        for (i in 0 until n - 1) {
+            for (j in 0 until n - i - 1) {
+                if (array[j] > array[j + 1]) {
+                    val temp = array[j]
+                    array[j] = array[j + 1]
+                    array[j + 1] = temp
+                }
+            }
+        }
     }
 }
